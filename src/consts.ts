2b02a6a663f9482782c9ac506a7d3cfa574ff197
Heuluck <sonicth@foxmail.com>
2025-12-23 16:44:43 +0800

    lint: 更加人看的代码

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Lucky Blog';
-export const SITE_DESCRIPTION = '早上好';
+export const SITE_TITLE = "Lucky Blog";
+export const SITE_DESCRIPTION = "早上好";
